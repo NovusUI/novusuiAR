8f11bff7a469d96d05a04c553f1de715bf4864c7
Adebola <deybollar@gmail.com>
2023-03-03 16:09:54 +0100

    rotate on only y

--- text.js
+++ text.js
@@ -1,48 +1,42 @@
-// var scene = document.querySelector('a-scene');
-// var entity = document.querySelector('#entity1');
+var scene = document.querySelector('a-scene');
+var entity = document.querySelector('#entity1');
 
-// var touchStartX = 0;
-// var touchStartY = 0;
+var touchStartX = 0;
+var touchStartY = 0;
 
-// scene.addEventListener('touchstart', onTouchStart);
-// scene.addEventListener('touchmove', onTouchMove);
-// scene.addEventListener('touchend', onTouchEnd);
+scene.addEventListener('touchstart', onTouchStart);
+scene.addEventListener('touchmove', onTouchMove);
+scene.addEventListener('touchend', onTouchEnd);
 
-// function onTouchStart(event) {
-//   touchStartX = event.touches[0].clientX;
-//   touchStartY = event.touches[0].clientY;
-// }
+function onTouchStart(event) {
+  touchStartX = event.touches[0].clientX;
+  touchStartY = event.touches[0].clientY;
+}
 
-// function onTouchMove(event) {
-//   var touchX = event.touches[0].clientX;
-//   var touchY = event.touches[0].clientY;
+function onTouchMove(event) {
+  var touchX = event.touches[0].clientX;
+  var touchY = event.touches[0].clientY;
   
-//   var deltaX = touchX - touchStartX;
-//   var deltaY = touchY - touchStartY;
+  var deltaX = touchX - touchStartX;
+  var deltaY = touchY - touchStartY;
+  var deltaZ = event.touches[0].pageY - touchY;
   
-//   // rotate entity
-//   var currentRotation = entity.getAttribute('rotation');
-//   var newX = currentRotation.x - deltaY / 2;
-//   var newY = currentRotation.y - deltaX / 2;
-//   var newZ = currentRotation.z;
-//   console.log(newZ)
-//   entity.setAttribute('rotation', {x: newX, y: newY, z: 0});
+  // rotate entity
+  var currentRotation = entity.getAttribute('rotation');
+  var newX = currentRotation.x - deltaY / 2;
+  var newY = currentRotation.y - deltaX / 2;
+  var newZ = currentRotation.z;
+
+  entity.setAttribute('rotation', {x: newX, y: newY, z: 0});
   
-//   // update touch start position
-//   touchStartX = touchX;
-//   touchStartY = touchY;
-// }
+  // update touch start position
+  touchStartX = touchX;
+  touchStartY = touchY;
+}
 
-// function onTouchEnd(event) {
-//     touchStartX = null;
-//   touchStartY = null;
-//   // cleanup code
-// }
+function onTouchEnd(event) {
+    touchStartX = null;
+  touchStartY = null;
+  // cleanup code
+}
 
-
-EightTouch.setup();
-      var myEntity = document.getElementById('entity1');
-      EightTouch.on('drag', myEntity, function(event) {
-        myEntity.object3D.position.x += event.detail.deltaX;
-        myEntity.object3D.position.y += event.detail.deltaY;
-      });
